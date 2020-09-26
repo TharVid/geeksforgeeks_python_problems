330dd10dd86c787c55149e85a406d20122d5baad
SUNIL KUMAR <50536429+TharVid@users.noreply.github.com>
2020-09-26 16:08:36 +0530

    hey

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # geeksforgeeks_python_problems
+Let's solve GFG python problems togather.
---
+++ add_two_number.py
@@ -0,0 +1,5 @@
+#add two numbers
+num1= input("Enter First No: ")
+num2= input("Enter Secound No: ")
+result= float(num1)+float(num2)
+print(f"Addition of {num1} and {num2} is {result} ")
