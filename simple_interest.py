86f829bc441b60ac41be7e01e13a839e409649cf
SUNIL KUMAR <50536429+TharVid@users.noreply.github.com>
2020-09-26 19:31:57 +0530

    Update

---
+++ simple_interest.py
@@ -0,0 +1,5 @@
+principal=float(input("Enter Proncipal Amount "))
+rate=float(input("Enter Annual Rate "))
+time= float(input("Enter time in years "))
+final_amount= float (principal*(1+((rate/100)*time)))
+print(final_amount)
