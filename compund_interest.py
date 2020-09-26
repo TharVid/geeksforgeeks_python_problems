e38853c1582a888f6de361ec244c7966c9b078af
SUNIL KUMAR <50536429+TharVid@users.noreply.github.com>
2020-09-26 20:13:49 +0530

    update

--- compund_interest.py
+++ compund_interest.py
@@ -1,6 +1,6 @@
-#simple intrest Simple Interest = (P x T x R)/100
+#compound intrest A = P(1 + R/100) t
 principal=float(input("Enter Proncipal Amount "))
 rate=float(input("Enter Annual Rate "))
 time= float(input("Enter time in years "))
-final_amount= float (principal*time*rate)/100
+final_amount= float (principal*(1+((rate/100)*time)))
 print(final_amount)
